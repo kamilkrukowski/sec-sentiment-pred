6182ae75f36695ddc2f2e4f814d3b5822643fdf6
kelseyyew <kelseyyew@gmail.com>
2023-03-01 14:19:58 -0500

    update to generate_labels.py

--- generate_labels.py
+++ generate_labels.py
@@ -2,28 +2,55 @@ import numpy as np
 import pandas as pd
 from datetime import datetime, timedelta
 import yfinance as yf
+import pickle
+
 
 def get_tikrs(file_dir):
+    '''
+    Reads in the TIKRS from a txt file separted by newlines
+    '''
     with open(file_dir) as f:
         tikrs = f.read().splitlines()
     return tikrs
 
 def load_data(tikrs, moving_period):
+    '''
+    Loads in historical data by tikr into a dictionary
+    tikrs and calculates specified avg moving period
+    '''
     TIKR_dict = {}
     for tikr in tikrs:
-        data_dir = str('historical/' + tikr + str('.csv'))
-        company_df = clean_df(data_dir, moving_period)
+        company_df = load_df(tikr, moving_period)
         TIKR_dict[tikr] = company_df
     return TIKR_dict
 
 
-def clean_df(tikr, moving_avg_d):
+def load_df(tikr, moving_avg):
+    '''
+    Downloads and clean dataframe and calculates specified avg moving period
+    '''
     df = yf.download(tikr, start="1990-01-01").reset_index()
     df['Close'] = df['Close'].replace('[\$,]', '', regex=True).astype(float)
-    df['moving_avg'] = df['Close'].rolling(window=moving_avg_d).mean()
+    df['moving_avg'] = df['Close'].rolling(window=moving_avg).mean()
     return df
 
 def label_performance(tikr, ref_df, date, days_period, threshold=0):
+    '''
+    Assigns Label to TIKR and Date
+
+    Parameters
+        ---------
+        tikr: str
+            a company identifier
+        ref_df: pd.df
+            reference for market performance (S&P 500)
+        date: int
+            date of document to label
+        days_period: int
+            number of trading days after date to compare performance
+        threshold: int
+            percent more or less to label underperformance or overperformance
+    '''
     try:
         date = datetime.strptime(str(date), "%Y%m%d.%f")
     except:
@@ -49,19 +76,37 @@ def label_performance(tikr, ref_df, date, days_period, threshold=0):
         else:
             return 2 # neutral performance
     except:
-        return -1
+        return -1 # invalid date or error
+
+def load_historical_data(filename):
+    with open(filename, 'rb') as handle:
+        tikr_dict = pickle.load(handle)
+        return tikr_dict
+
+def output_historical_data(filename, data):
+    with open(filename, 'wb') as handle:
+        pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
-tikrs = get_tikrs('100_tikrs.txt')
-TIKRS_dat = load_data(tikrs, 7)
-comp_his = clean_df('^GSPC', 7)
+'''
+DOWNLOADS TIKR DATA TO STORE
+Only run if TIKR_DATA.pickle does not exist
+'''
+# tikrs = get_tikrs('100_tikrs.txt')
+# TIKRS_dat = load_data(tikrs, 7)
+# output_historical_data('TIKR_DATA.pickle', TIKRS_dat)
+
+#####################################################
+
+# loads historical dataframe from pickle file
+TIKRS_dat = load_historical_data('TIKR_DATA.pickle')
+comp_his = load_df('^GSPC', 7) # reference dataframe
 
 
-data = pd.read_csv('8k_data.csv')
+data = pd.read_csv('8k_dataset.csv')
 data['label'] = data.apply(lambda row: label_performance(
-            row['tikr'], comp_his, row['Date'], 7, 0.01), axis=1)
+            row['tikr'], comp_his, row['date'], 7, 0.01), axis=1)
 
+print(data['label'].value_counts())
 data.to_csv('8k_data_labels.csv')
 
-
-# exporting to pickle https://stackoverflow.com/questions/11218477/how-can-i-use-pickle-to-save-a-dict-or-any-other-python-object
