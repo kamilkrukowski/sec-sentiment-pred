6ae3629d3d6db889147868e091789c61b2a96e9e
Kamil Krukowski <Kamiljkrukowski@gmail.com>
2023-03-07 14:47:00 -0500

    Use new edgar-doc-parser DataLoader + Parser_8K

--- generate_data.py
+++ generate_data.py
@@ -1,54 +1,52 @@
-import sys
-
-import edgar.html as html
 import edgar
+from .parser_8k import Parser_8K
 
 DATA_DIR = 'data'
-DELETE_RAW = True
+# Whether to include type of 8-K document in embedding.
+USE_DOC_SUBTYPES = True
 
 tikrs = ['aapl', 'msft', 'amzn', 'tsla', 'googl', 'goog',  'unh', 'jnj', 'cvx',
          'jpm', 'hd', 'v', 'pg']
-metadata = edgar.Metadata(data_dir=DATA_DIR)
 
 #  Prepare file for constant offloading
-fout = open('8k_data.csv', 'w');
+fout = open('8k_data.csv', 'w')
 fout.write(",tikr,FORM_TYPE,submission,text,Date\n")
 frowidx = 0
 
+config = edgar.DataLoaderConfig(
+    force_remove_raw=True, include_supplementary=False,
+    return_submission=True, return_tikr=True)
+dataloader = edgar.DataLoader(tikrs, document_type='8-K', data_dir=DATA_DIR,
+                              config=config)
+
+# Extract presence of headers
+parser = Parser_8K()
 
 data = []
-for tikr in tikrs:
-    print(tikr)
-    edgar.load_files(tikr, data_dir=DATA_DIR, document_type='8-K',
-                     include_supplementary=True, force_remove_raw=DELETE_RAW)
+for idx, tikr, sub, text in enumerate(dataloader):
 
-    submissions = metadata.get_submissions(tikr)
-    for sub in submissions:
-        files = edgar.get_files(tikrs=tikr, submissions=sub, metadata=metadata)
+    attrs = dataloader.metadata._get_submission(tikr, sub)['attrs']
+    # Remove submissions without date-time
+    if 'CONFORMED PERIOD OF REPORT' not in attrs:
+        continue
+    date = attrs['CONFORMED PERIOD OF REPORT']
 
-        # Store consecutive supplementary file contents as text stream to
-        # concatenate later.
-        out = []
-        for file in files:
-            # Skip unextracted files
-            if not metadata._get_file(tikr, sub, file).get('extracted', False):
-                continue;
+    doc_type = '8-K'
+    if USE_DOC_SUBTYPES:
+        # Label the type of 8-K Document
+        subtypes = parser.get_section_types(text)
+        subtype = None
+        if len(subtypes) == 0:
+            subtype = 'None'
+        elif len(subtypes) == 1:
+            subtype = subtypes[0]
+        else:
+            subtype = 'MULTI'
+        doc_type = f'8-K-{subtype}'
 
-            f = edgar.read_file(tikr, sub, file, document_type='8-K',
-                                data_dir=DATA_DIR)
-            out.append(f)
-
-        entry = html.clean_text(html.remove_tables(' '.join(out)))
-
-        attrs = metadata._get_submission(tikr, sub)['attrs']
-        # Remove submissions without date-time
-        if 'CONFORMED PERIOD OF REPORT' not in attrs:
-            continue;
-        date = attrs['CONFORMED PERIOD OF REPORT']
-
-        # Write to file
-        fout.write(f"{frowidx},{tikr},8-K,{sub},{entry},{date}\n")
-        frowidx += 1
+    # Write to file
+    fout.write(f"{frowidx},{tikr},{doc_type},{sub},{text},{date}\n")
+    frowidx += 1
 
 #  Close data pipe file
 fout.close()
