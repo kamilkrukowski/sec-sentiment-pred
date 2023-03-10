f17f01824e477677d445c49b92203a808fcf32f1
Kamil Krukowski <Kamiljkrukowski@gmail.com>
2023-03-10 17:17:47 -0500

    Started trading strategies implementation

---
+++ strategies.py
@@ -0,0 +1,12 @@
+from typing import List, Dict
+from datetime import datetime
+
+def baseline(
+        predictions: List[tuple],
+        company_list: List[str]
+            ) -> Dict[datetime, List[float]]:
+    #Calculate portfolio holdings at time intervals given 8-K labels.
+
+    strategy = dict()
+
+    return strategy
