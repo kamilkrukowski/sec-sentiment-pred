815f1f952aa97294fb5cb4260dcc55ef39e8443e
Kamil Krukowski <Kamiljkrukowski@gmail.com>
2023-03-10 17:56:57 -0500

    Added chronosort to eval_strategy

--- eval_strategy.py
+++ eval_strategy.py
@@ -1,10 +1,53 @@
-from . import strategies
+from datetime import datetime
+
+
+import numpy as np
+
+
+import strategies
 from portfolio_eval import get_strategy_annual_return
+
+
+START_DATE = datetime.strptime('20000101', '%Y%m%d')
 
 strategy = strategies.baseline
 
-labels = [int(i.split('\t')[1]) for i in open('train.tsv').readlines()]
-company_list = open('tickrs.txt', 'r').read().strip().split('\n')
+labels = []
+dates = []
+with open('train.tsv') as f:
+    f.readline()  # Skip HEADER
+    for line in f.readlines():
+        _, label, date = line.strip().split('\t')
+        dates.append(datetime.strptime(date, '%Y%m%d'))
+        labels.append(int(label))
+
+def chronosort(dates, arr, start_date: datetime):
+    dates = np.array(dates)
+    labels = np.array(labels)
+
+    sort_idxs = np.argsort(dates)
+    labels = labels[sort_idxs]
+    dates = dates[sort_idxs]
+
+    dates = list(dates)
+    dates.insert(0, START_DATE)
+    labels = list(labels)
+    labels.insert(0, -999)
+
+    sort_idxs = list(np.argsort(dates))
+    found_idx = sort_idxs.index(0) + 1  # We exclude our dummy entry
+
+    dates = np.array(dates)
+    labels = np.array(labels)
+    dates = dates[sort_idxs][found_idx:]
+    labels = labels[sort_idxs][found_idx:]
+
+    return dates, labels
+
+
+dates, labels = chronosort(dates, labels, START_DATE)
+
+company_list = open('tikrs.txt', 'r').read().strip().split('\n')
 
 allocations = strategy(labels, company_list)
 
