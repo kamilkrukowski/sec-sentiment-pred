2eea16a6c475fee5aaaf840ed5712cea931415a7
kelseyyew <kelseyyew@gmail.com>
2023-04-05 10:34:47 -0400

    k-folds and validation

--- eval_model.py
+++ eval_model.py
@@ -20,7 +20,7 @@ HOLD_PERIOD=90
 
 EVAL_YEAR = 2018
 EVAL_YEAR = datetime.strptime(str(EVAL_YEAR) + '0101', '%Y%m%d')
-model_savepath = 'test2'
+model_savepath = 'test1'
 
 K = 3
 # Load data
@@ -40,15 +40,16 @@ model = GET_BOW_RESULTS
 all_metrics = []
 output_df = pd.DataFrame()
 N_YEARS = 2023-EVAL_YEAR.year+1
-for idx, (traindf, validationdf, testdf) in enumerate(
+for idx, (train_val_df, testdf) in enumerate(
             generate_data_splits(data, strategy=strategy,
                                  periods_to_test=N_YEARS)):
 
     # x_train, y_train = traindf['text'], traindf['label']
     # x_test, y_test = testdf['text'], testdf['label']
     
-    for k, (train_index, test_index) in enumerate(KFold(n_splits=K, shuffle=True).split(traindf)):
-        traindf_k = traindf.iloc[train_index]
+    for k, (train_index, test_index) in enumerate(KFold(n_splits=K, shuffle=True).split(train_val_df)):
+        traindf = train_val_df.iloc[train_index]
+        validationdf = train_val_df.iloc[test_index]
 
         """
         total_test = len(y_test)
@@ -59,7 +60,7 @@ for idx, (traindf, validationdf, testdf) in enumerate(
             print(f"{label} - test: {prop_test:0.4f}, train: {prop_train:0.4f}")
         """
 
-        out, metrics = model(traindf_k, validationdf, testdf)
+        out, metrics = model(traindf, validationdf, testdf)
         out['Date'] = pd.to_datetime(out['Date'], format="%Y%m%d")
         metrics.year = testdf.Date.iloc[0].year
         metrics.k = k + 1
@@ -67,7 +68,7 @@ for idx, (traindf, validationdf, testdf) in enumerate(
         print(metrics)
         all_metrics.append(metrics)
 
-        # get_reference_data(out, yd, cols=['Annual Return', 'beta', 'sp Annual', 'sp Percent'])
+        get_reference_data(out, yd, cols=['Annual Return', 'beta', 'sp Annual', 'sp Percent'])
 
         temp_df = pd.DataFrame()
         temp_df[['tikr', 'Date','label', 'pred', 'score']] = out[['tikr', 'Date','label', 'pred', 'score']]
@@ -82,25 +83,18 @@ if not os.path.exists('figs'):
 output_df.reset_index(drop=True).to_csv(f'model_outputs/outputs_{model_savepath}.csv')
 """
 out = out[out.beta != -999]
-
 any_adv_ = (out['Annual Return'] - out['sp Annual']).mean()
 any_alph = ((out['Annual Return'] - out['sp Annual'])/out['beta']).mean()
-
 out_ = out[out.pred == 1]
 pos_adv_ = (out_['Annual Return'] - out_['sp Annual']).mean()
 pos_alph = ((out_['Annual Return'] - out_['sp Annual'])/out_['beta']).mean()
-
 print(f"Any: Return: {out['Annual Return'].mean():.2f}, S&P500: {out['sp Annual'].mean():.2f}"
     f" Beta: {out['beta'].mean():.2f}")
 print(f"Pos: Return: {out_['Annual Return'].mean():.2f}, S&P500: {out_['sp Annual'].mean():.2f}"
     f" Beta: {out_['beta'].mean():.2f}")
-
 sharpe_sp = out['sp Annual'].mean()/out['sp Annual'].std() 
 sharpe_any = out['Annual Return'].mean()/out['Annual Return'].std() 
 sharpe_pos = out_['Annual Return'].mean()/out_['Annual Return'].std() 
 print(f"Sharpe Ratios: S&P500: {sharpe_sp:.2f} Any: {sharpe_any:.2f} Pos: {sharpe_pos:.2f}")
-
 cols = ['Date', 'label', 'pred', 'score', 'tikr']
 out[cols].to_csv('results_bow.tsv', sep='\t')
-"""""
-
